71cb953cd259708d28d5b282e32068c684c6d8cf
Johnny <101834686+Johnny-DF26@users.noreply.github.com>
2025-11-04 15:13:52 -0300

    up app.py

--- API/app.py
+++ API/app.py
@@ -1359,7 +1359,7 @@ def atualizar_status(id_relatorio):
 # Rota Home
 # ------------------------
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
 
 
 #-------------------------
