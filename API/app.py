36f1068025606bbe8c8c82a50d29c4dcf0a1783a
Johnny <101834686+Johnny-DF26@users.noreply.github.com>
2025-11-05 20:41:05 -0300

    activate online

--- API/app.py
+++ API/app.py
@@ -1441,8 +1441,8 @@ def atualizar_status(id_relatorio):
 # Rota Home
 # ------------------------
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
-    #app.run(host='0.0.0.0', port=8080, debug=True)
+    #app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
 #-----------------------------
 # Teste
 #-----------------------------
