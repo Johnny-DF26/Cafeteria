da2c9a68732b82320764c0dbbe7559c159315b9a
Johnny <101834686+Johnny-DF26@users.noreply.github.com>
2025-11-06 23:22:08 -0300

    update

--- API/app.py
+++ API/app.py
@@ -1592,8 +1592,8 @@ def atualizar_status(id_relatorio):
 # Rota Home
 # ------------------------
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True) # Local
-    #app.run(host='0.0.0.0', port=8080, debug=True) # Online
+    app.run(host='0.0.0.0', port=8080, debug=True) # Online
+    #app.run(host='0.0.0.0', port=5000, debug=True) # Local
 #-----------------------------
 # Teste
 #-----------------------------
--- API/db.py
+++ API/db.py
@@ -5,7 +5,7 @@ import os
 
 load_dotenv()  # lê o .env
 
-def get_connection(): # Conexão com o banco de dados MySQL localmente. Obs mudar para connection
+def get_connection2(): # Conexão com o banco de dados MySQL localmente. Obs mudar para connection
     try:
         connection = mysql.connector.connect(
             host=os.getenv('DB_HOST_LOCAL'),
@@ -23,7 +23,7 @@ def get_connection(): # Conexão com o banco de dados MySQL localmente. Obs muda
     
 
 
-def get_connection2(): # Conexão com o banco de dados MySQL no Railway
+def get_connection(): # Conexão com o banco de dados MySQL no Railway
     try:
         connection = mysql.connector.connect(
             host=os.getenv('DB_HOST'),
